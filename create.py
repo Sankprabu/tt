7042a8a953620309c60f9643294808569cae0e03
Sankprabu <103571923+Sankprabu@users.noreply.github.com>
2024-05-10 10:57:43 +0700

    Update create.py

--- create.py
+++ create.py
@@ -11,6 +11,8 @@ import requests
 import ua_generator
 import threading
 import time
+import random
+from datetime import datetime
 name = open("lib/nama_indonesia").read().splitlines()
 
 # boleh ditambah asal jangan di apus id punya gue
@@ -46,72 +48,133 @@ def cvs(cookie):
 
 
 
+
+
 class Create:
     
-    def __init__(self, name, mail, birthday):
+    def __init__(self, name, email, phone_number, birthday):
         self.ses = requests.Session()
         self.name = name
-        self.mail = mail
+        self.email = email
+        self.phone_number = phone_number
         self.birthday = birthday
-        self.password = kontol["password"]  # Pastikan variabel kontol sudah didefinisikan sebelumnya
-        self.ses.headers.update({"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7", "upgrade-insecure-requests": "1", "user-agent": user_agent})  # Pastikan variabel user_agent sudah didefinisikan sebelumnya
-        self.ses.headers.update({"sec-fetch-site": "none", "sec-fetch-mode": "navigate", "sec-fetch-dest": "document", "viewport-width": "2756", "sec-ch-prefers-color-scheme": "light"})
+        # Anda perlu menentukan bagaimana mendapatkan password atau mengganti 'kontol["password"]' dengan metode yang sesuai
+        self.password = kontol["password"]
+        # Update user_agent dengan nilai yang sesuai
+        self.user_agent = user_agent
+        self.ses.headers.update({
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+            "upgrade-insecure-requests": "1",
+            "user-agent": self.user_agent
+        })
+        self.ses.headers.update({
+            "sec-fetch-site": "none",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-dest": "document",
+            "viewport-width": "2756",
+            "sec-ch-prefers-color-scheme": "light"
+        })
+        # Jika menggunakan kontol manual, sesuaikan dengan kondisi yang sesuai
         if len(kontol["manual"]) <= 1: 
-            self.ses.headers.update({"sec-ch-ua": sechuafull.ch.brands.replace('" Not A;', '"Not.A/'), "sec-ch-ua-mobile": sechuafull.ch.mobile, "sec-ch-ua-platform-version": sechuafull.ch.platform_version, "sec-ch-ua-full-version-list": sechuafull.ch.brands_full_version_list.replace('" Not A;', '"Not.A/'), "sec-ch-ua-platform": sechuafull.ch.platform})
+            self.ses.headers.update({
+                "sec-ch-ua": sechuafull.ch.brands.replace('" Not A;', '"Not.A/'),
+                "sec-ch-ua-mobile": sechuafull.ch.mobile,
+                "sec-ch-ua-platform-version": sechuafull.ch.platform_version,
+                "sec-ch-ua-full-version-list": sechuafull.ch.brands_full_version_list.replace('" Not A;', '"Not.A/'),
+                "sec-ch-ua-platform": sechuafull.ch.platform
+            })
         self.res = self.ses.get("https://m.facebook.com/")
     
     @property
     def fetch(self):
+        # Mengambil URL pendaftaran melalui email
         self.res = self.ses.get("https://m.facebook.com" + BeautifulSoup(self.res.text, "html.parser").find("a", id="signup-button")["href"] + "&soft=hjk", headers={**self.ses.headers, "referer": self.res.url})
         self.par = BeautifulSoup(self.res.text, "html.parser")
         self.form = self.par.find("form", id="mobile-reg-form")
         self.ses.headers.update({"referer": self.res.url, "host": "m.facebook.com"})
-        return {"ccp": self.form.find("input", {"name": "ccp"})["value"], "reg_instance": self.form.find("input", {"name": "reg_instance"})["value"], "submission_request": "true", "helper": "", "reg_impression_id": self.form.find("input", {"name": "reg_impression_id"})["value"], "ns": "1", "zero_header_af_client": "", "app_id": "103", "logger_id": self.form.find("input", {"name": "logger_id"})["value"], "field_names[0]": "firstname", "firstname": self.name, "field_names[1]": "birthday_wrapper", "birthday_day": self.birthday["day"], "birthday_month": self.birthday["month"], "birthday_year": self.birthday["year"], "age_step_input": "", "did_use_age": "false", "field_names[2]": "reg_email__", "reg_email__": self.mail["mail"], "field_names[3]": "sex", "sex": random.SystemRandom().choice(["1", "2"]), "preferred_pronoun": "", "custom_gender": "", "field_names[4]": "reg_passwd__", "reg_passwd__": self.password, "name_suggest_elig": "false", "was_shown_name_suggestions": "false", "did_use_suggested_name": "false", "use_custom_gender": "false", "guid": "", "pre_form_step": "", "encpass": "", "submit": "Daftar", "fb_dtsg": re.search('"dtsg":{"token":"(.*?)"', self.res.text).group(1), "jazoest": self.form.find("input", {"name": "jazoest"})["value"], "lsd": self.form.find("input", {"name": "lsd"})["value"], "__dyn": "", "__csr": "", "__req": random.choice("qwertyuiopasdfghjklzxcvbnm"), "__a": re.search('"encrypted":"(.*?)"', self.res.text).group(1), "__user": "0"}, self.form["action"]
+        return {
+            "ccp": self.form.find("input", {"name": "ccp"})["value"],
+            "reg_instance": self.form.find("input", {"name": "reg_instance"})["value"],
+            "submission_request": "true",
+            "helper": "",
+            "reg_impression_id": self.form.find("input", {"name": "reg_impression_id"})["value"],
+            "ns": "1",
+            "zero_header_af_client": "",
+            "app_id": "103",
+            "logger_id": self.form.find("input", {"name": "logger_id"})["value"],
+            "field_names[0]": "firstname",
+            "firstname": self.name,
+            "field_names[1]": "birthday_wrapper",
+            "birthday_day": self.birthday["day"],
+            "birthday_month": self.birthday["month"],
+            "birthday_year": self.birthday["year"],
+            "age_step_input": "",
+            "did_use_age": "false",
+            "field_names[2]": "reg_email__",
+            # Menggunakan email sebagai alamat email
+            "reg_email__": self.email,
+            "field_names[3]": "sex",
+            "sex": random.SystemRandom().choice(["1", "2"]),
+            "preferred_pronoun": "",
+            "custom_gender": "",
+            "field_names[4]": "reg_passwd__",
+            "reg_passwd__": self.password,
+            "name_suggest_elig": "false",
+            "was_shown_name_suggestions": "false",
+            "did_use_suggested_name": "false",
+            "use_custom_gender": "false",
+            "guid": "",
+            "pre_form_step": "",
+            "encpass": "",
+            "submit": "Daftar",
+            "fb_dtsg": re.search('"dtsg":{"token":"(.*?)"', self.res.text).group(1),
+            "jazoest": self.form.find("input", {"name": "jazoest"})["value"],
+            "lsd": self.form.find("input", {"name": "lsd"})["value"],
+            "__dyn": "",
+            "__csr": "",
+            "__req": random.choice("qwertyuiopasdfghjklzxcvbnm"),
+            "__a": re.search('"encrypted":"(.*?)"', self.res.text).group(1),
+            "__user": "0"
+        }, self.form["action"]
     
-    def register_phone(self):
+    def register(self):
         self.data, self.action = self.fetch
         self.ses.post("https://m.facebook.com" + self.action, data=self.data, headers={**self.ses.headers, "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "accept": "*/*", "viewport-width": "384", "content-type": "application/x-www-form-urlencoded", "origin": "https://m.facebook.com", "x-asbd-id": "129477", "x-fb-lsd": self.data["lsd"], "cache-control": "max-age=0"})
         self.res = self.ses.get(f"https://m.facebook.com/login/save-device/?login_source=account_creation&logger_id={self.data['logger_id']}&app_id=103", headers={**self.ses.headers, "sec-fetch-site": "same-origin"})
         if "checkpoint" in self.res.url:
             print(" [!] oops checkpoint")
-            print(f" [!] email: {self.mail['mail']}")
+            print(f" [!] email: {self.email}")
             print(f" [!] useragent: {user_agent}")
             return "CP-MANG"
         self.ses.headers.update({"referer": self.res.url})
         self.par = BeautifulSoup(self.res.text, "html.parser")
         self.form = self.par.find("form", action=re.compile("^/login/device-based/update-nonce/"))
         self.res = self.ses.post("https://m.facebook.com" + self.form["action"], data={i["name"]: i["value"] for i in self.form.find_all("input", {"name": True, "value": True})}, headers={**self.ses.headers, "sec-fetch-user": "?1", "sec-fetch-site": "same-origin", "content-type": "application/x-www-form-urlencoded", "origin": "https://m.facebook.com", "cache-control": "max-age=0"})
-        self.data = {"fb_dtsg": re.search('"dtsg":{"token":"(.*?)"', self.res.text).group(1), "jazoest": re.search('"jazoest", "(\d*)"', self.res.text).group(1), "lsd": re.search('LSD",\[\],{"token":"(.*?)"', self.res.text).group(1), "__dyn": "", "__csr": "", "__req": "4", "__a": re.search('"encrypted":"(.*?)"', self.res.text).group(1), "__user": self.ses.cookies["c_user"]}
+        self.data = {
+            "fb_dtsg": re.search('"dtsg":{"token":"(.*?)"', self.res.text).group(1),
+            "jazoest": re.search('"jazoest", "(\d*)"', self.res.text).group(1),
+            "lsd": re.search('LSD",\[\],{"token":"(.*?)"', self.res.text).group(1),
+            "__dyn": "",
+            "__csr": "",
+            "__req": "4",
+            "__a": re.search('"encrypted":"(.*?)"', self.res.text).group(1),
+            "__user": self.ses.cookies["c_user"]
+        }
     
-    def verifikasi_phone(self, kode):
+    def verifikasi(self, kode):
+        # Melakukan verifikasi melalui kode yang diterima melalui email
         self.ses.headers.update({"referer": self.res.url})
-        self.res = self.ses.post(f"https://m.facebook.com/confirmation_cliff/?contact={self.mail['mail'].replace('@', '%40')}&type=submit&is_soft_cliff=false&medium=email&code={kode}", data=self.data, headers={**self.ses.headers, "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "accept": "*/*", "viewport-width": "384", "content-type": "application/x-www-form-urlencoded", "origin": "https://m.facebook.com", "x-asbd-id": "129477", "x-fb-lsd": self.data["lsd"]})
+        self.res = self.ses.post(f"https://m.facebook.com/confirmation_cliff/?contact={self.email.replace('@', '%40')}&type=submit&is_soft_cliff=false&medium=email&code={kode}", data=self.data, headers={**self.ses.headers, "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "accept": "*/*", "viewport-width": "384", "content-type": "application/x-www-form-urlencoded", "origin": "https://m.facebook.com", "x-asbd-id": "129477", "x-fb-lsd": self.data["lsd"]})
         if "home.php?confirmed_account" in self.res.text:
             self.ses.get("https://m.facebook.com/home.php?confirmed_account")
-        self.createat = __import__('datetime').datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        self.createat = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         self.ses.headers.update({"host": "mbasic.facebook.com"})
         self.res = self.ses.get("https://mbasic.facebook.com/profile.php")
         self.par = BeautifulSoup(self.res.text, "html.parser")
         self.form = self.par.find("form", method="post")
         self.res = self.ses.post("https://mbasic.facebook.com" + self.form["action"], data={i["name"]: i["value"] for i in self.form.find_all("input", {"name": True, "value": True})}, headers={**self.ses.headers, "sec-fetch-user": "?1", "sec-fetch-site": "same-origin", "content-type": "application/x-www-form-urlencoded", "origin": "https://mbasic.facebook.com", "cache-control": "max-age=0"})
         print(" [*] berhasil membuat akun")
-
-    def register_email(self):
-        # Lakukan pendaftaran menggunakan email di sini
-        pass
-    
-    def verifikasi_email(self, kode):
-        # Lakukan verifikasi menggunakan email di sini
-        pass
-    
-    def register(self):
-        self.register_phone()  # Lakukan pendaftaran menggunakan nomor telepon
-        # Tunggu selama 3 detik
-        time.sleep(3)
-        # Jika tidak menerima kode dalam 3 detik, lanjutkan dengan pendaftaran melalui email
-        if not self.verifikasi_phone(kode):
-            self.register_email()  # Lakukan pendaftaran menggunakan email
-
 
 
 		
