9a0489427af6dfd85c485d727c854f30653d20bb
Sankprabu <103571923+Sankprabu@users.noreply.github.com>
2024-05-11 23:02:11 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -8,17 +8,27 @@ login_url = 'https://m.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&ref
 with open('akun.txt', 'r') as file:
     email, password = file.readline().strip().split(',')
 
-# Data yang diperlukan untuk login
-data = {
-    'email': email,
-    'pass': password
-}
-
 # Buat sesi HTTP menggunakan requests
 session = requests.Session()
 
+# Kirimkan permintaan GET untuk mendapatkan halaman login
+response = session.get(login_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Temukan form login
+form = soup.find('form')
+
+# Temukan input untuk email dan password
+email_input = form.find('input', {'name': 'email'})
+password_input = form.find('input', {'name': 'pass'})
+login_button = form.find('button', {'type': 'submit'})
+
+# Set nilai email dan password
+email_input['value'] = email
+password_input['value'] = password
+
 # Kirimkan permintaan POST untuk login
-response = session.post(login_url, data=data)
+response = session.post(login_url, data={email_input['name']: email, password_input['name']: password})
 
 # Periksa apakah login berhasil
 if 'Logout' in response.text:
