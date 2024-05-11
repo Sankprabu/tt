7e4fb3dd52939f55c6668fa132950b58d7115f50
Sankprabu <103571923+Sankprabu@users.noreply.github.com>
2024-05-11 21:54:42 +0700

    Update laz.py

--- laz.py
+++ laz.py
@@ -1,7 +1,9 @@
+import os
 from selenium import webdriver
 
-# Path ke Chromedriver
-chrome_driver_path = '/storage/emulated/0/chromedriver'
+# Set environment variable PATH to directory containing Chromedriver
+chrome_driver_path = '/storage/emulated/0/'
+os.environ['PATH'] = f'{chrome_driver_path}:{os.environ["PATH"]}'
 
 # URL login mobile Facebook
 facebook_mobile_login_url = 'https://m.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8'
@@ -16,8 +18,8 @@ with open('akun.txt', 'r') as file:
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-# Initialize Chrome WebDriver dengan path Chromedriver yang ditentukan
-driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome(options=chrome_options)
 
 # Open Facebook login page
 driver.get(facebook_mobile_login_url)
