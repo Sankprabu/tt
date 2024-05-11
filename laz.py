73df3478a24b79c5937fcb0b2f89730e290a7f8b
Sankprabu <103571923+Sankprabu@users.noreply.github.com>
2024-05-11 08:15:11 +0700

    Update laz.py

--- laz.py
+++ laz.py
@@ -1,10 +1,15 @@
+import json
+import random
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from fake_useragent import UserAgent
+import os
 
 # Path to your webdriver
-webdriver_path = '/path/to/chromedriver'
+webdriver_path = '/storage/emulated/0/chromedriver'
 
 # URL login mobile Facebook
 facebook_mobile_login_url = 'https://m.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8'
@@ -15,12 +20,27 @@ facebook_gdpr_consent_url = 'https://www.facebook.com/privacy/consent/gdp/?param
 # URL login Lazada dengan Facebook
 lazada_login_url = 'https://member.lazada.co.id/user/login'
 
-# Data akun Facebook
-with open('akun.txt', 'r') as file:
-    email, password = file.readline().strip().split()
+# Path to your JSON file containing user-agents
+user_agents_file = 'juragan.json'
 
-# Start a new instance of Chrome browser
-driver = webdriver.Chrome(webdriver_path)
+# Check if juragan.json is empty or does not exist
+if not os.path.exists(user_agents_file) or os.path.getsize(user_agents_file) == 0:
+    # Generate random user-agent
+    user_agent = UserAgent().random
+else:
+    # Read user-agents from JSON file
+    with open(user_agents_file, 'r') as file:
+        user_agents_data = json.load(file)
+    
+    # Randomly select a user-agent from the manual list
+    user_agent = random.choice(user_agents_data['manual'])
+
+# Configure Chrome options with the selected user-agent
+chrome_options = Options()
+chrome_options.add_argument(f'user-agent={user_agent}')
+
+# Start a new instance of Chrome browser with the configured options
+driver = webdriver.Chrome(webdriver_path, options=chrome_options)
 
 try:
     # Login to Facebook
