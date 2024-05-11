7709bdf3baa79d40252a3dfbe37aa53469f8f425
Sankprabu <103571923+Sankprabu@users.noreply.github.com>
2024-05-11 23:05:36 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,37 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL login mobile Facebook
-login_url = 'https://m.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8'
+def login_to_facebook(email, password):
+    # URL login mobile Facebook
+    login_url = 'https://m.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8'
+
+    # Buat sesi HTTP menggunakan requests
+    session = requests.Session()
+
+    # Kirimkan permintaan GET untuk mendapatkan halaman login
+    response = session.get(login_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Temukan form login
+    form = soup.find('form')
+
+    # Temukan input untuk email dan password
+    email_input = form.find('input', {'name': 'email'})
+    password_input = form.find('input', {'name': 'pass'})
+    login_button = form.find('button', {'type': 'submit'})
+
+    # Set nilai email dan password
+    email_input['value'] = email
+    password_input['value'] = password
+
+    # Kirimkan permintaan POST untuk login
+    response = session.post(login_url, data={email_input['name']: email, password_input['name']: password})
+
+    # Periksa apakah login berhasil
+    if 'Logout' in response.text:
+        print("Login berhasil!")
+    else:
+        print("Login gagal. Silakan periksa kembali email dan password Anda.")
 
 # Baca informasi email dan password dari file akun.txt
 with open('akun.txt', 'r') as file:
     email, password = file.readline().strip().split(',')
 
-# Buat sesi HTTP menggunakan requests
-session = requests.Session()
-
-# Kirimkan permintaan GET untuk mendapatkan halaman login
-response = session.get(login_url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# Temukan form login
-form = soup.find('form')
-
-# Temukan input untuk email dan password
-email_input = form.find('input', {'name': 'email'})
-password_input = form.find('input', {'name': 'pass'})
-login_button = form.find('button', {'type': 'submit'})
-
-# Set nilai email dan password
-email_input['value'] = email
-password_input['value'] = password
-
-# Kirimkan permintaan POST untuk login
-response = session.post(login_url, data={email_input['name']: email, password_input['name']: password})
-
-# Periksa apakah login berhasil
-if 'Logout' in response.text:
-    print("Login berhasil!")
-else:
-    print("Login gagal.")
+# Panggil fungsi login
+login_to_facebook(email, password)
